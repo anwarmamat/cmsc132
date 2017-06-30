c1dfa4f4c11cbd841d95b650956e8e7b210233de
Anwar Mamat <anwar@umd.edu>
2017-06-30 08:51:35 -0400

    adds lca

--- binarytree/BinaryTree.java
+++ binarytree/BinaryTree.java
@@ -310,6 +310,41 @@ public class BinaryTree<E> {
     }
     
     
+    public E findLCA(E n1, E n2)
+    {
+		Node t = findLCA(root, n1, n2);
+        return t.key;
+    }
+ 
+    // This function returns pointer to LCA of two given
+    // values n1 and n2. This function assumes that n1 and
+    // n2 are present in Binary Tree
+    Node findLCA(Node node, E n1, E n2)
+    {
+        // Base case
+        if (node == null)
+            return null;
+ 
+        // If either n1 or n2 matches with root's key, report
+        // the presence by returning root (Note that if a key is
+        // ancestor of other, then the ancestor key becomes LCA
+        if (node.key.equals(n1) || node.key.equals(n2))
+            return node;
+ 
+        // Look for keys in left and right subtrees
+        Node left_lca = findLCA(node.left, n1, n2);
+        Node right_lca = findLCA(node.right, n1, n2);
+ 
+        // If both of the above calls return Non-NULL, then one key
+        // is present in once subtree and other is present in other,
+        // So this node is the LCA
+        if (left_lca!=null && right_lca!=null)
+            return node;
+ 
+        // Otherwise check if left subtree or right subtree is LCA
+        return (left_lca != null) ? left_lca : right_lca;
+    }
+    
     /**
      * @param args the command line arguments
      */
